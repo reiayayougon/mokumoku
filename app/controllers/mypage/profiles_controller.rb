9c8d55d89df949eb177b93c4a456f2267ce39ba4
reiayayougon <ishida.ayako0306@gmail.com>
2023-07-07 01:33:55 +0900

    Revert "削除し忘れをrevert"
    This reverts commit 064ca35e120e2c2b05215d2096a66fe3b3654dd0.

--- app/controllers/mypage/profiles_controller.rb
+++ app/controllers/mypage/profiles_controller.rb
@@ -21,6 +21,6 @@ class Mypage::ProfilesController < Mypage::BaseController
   private
 
   def profile_params
-    params.require(:user).permit(:name, :avatar, :hobby, :profile)
+    params.require(:user).permit(:name, :avatar, :email, :hobby, :profile)
   end
 end
